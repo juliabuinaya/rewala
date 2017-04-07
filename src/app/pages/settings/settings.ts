59607cc3000c2886421921e2de165d2e3991986c
Dmitriy Melnichenko <tairezzzz@gmail.com>
2017-04-07 18:26:11 +0300

    update settings page

--- src/app/pages/settings/settings.ts
+++ src/app/pages/settings/settings.ts
@@ -9,9 +9,20 @@ import { NavController, IonicPage } from 'ionic-angular';
   templateUrl: 'settings.html'
 })
 export class SettingsPage {
+  public settings: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+      public navCtrl: NavController
+  ) {
 
+    this.settings = [
+      { label: 'Send daily emails', description: 'Emails about daily activity', value: false },
+      { label: 'Send push notifications', description: '', value: true }
+    ];
+  }
+
+  saveSettigns(event) {
+    console.log('settings saved');
   }
 
 }
