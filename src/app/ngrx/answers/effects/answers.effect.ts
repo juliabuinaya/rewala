8fcbc4d21bcf785beacad64017e012385a6d822c
Beliars <2beliars@gmail.com>
2017-06-20 17:04:58 +0300

    answer state effects update

--- src/app/ngrx/answers/effects/answers.effect.ts
+++ src/app/ngrx/answers/effects/answers.effect.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs/Observable';
 //actions
 import * as myAnswersGet from '../../answers-request/nested-states/my-answers-get/actions/my-answers-get.actions';
 import * as answerPost from '../../answers-request/nested-states/answer-post/actions/answer-post.actions';
+import * as multipleAnswersPost from '../../answers-request/nested-states/multiple-answers-post/actions/multiple-answers-post.actions';
 import { SetMyAnswersAction, UpdateMyAnswersAction } from '../actions/answers.actions';
 
 @Injectable()
@@ -22,6 +23,8 @@ export class AnswersEffects {
   
   @Effect()
   updateMyAnswers$: Observable<Action> = this.actions$
-  .ofType(answerPost.ActionTypes.REQUEST_SUCCESS)
+  .ofType(answerPost.ActionTypes.REQUEST_SUCCESS,
+          multipleAnswersPost.ActionTypes.REQUEST_SUCCESS)
   .map(action => new UpdateMyAnswersAction(toPayload(action)));
+  
 }
