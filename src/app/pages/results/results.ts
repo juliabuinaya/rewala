c378dbdda775deaffbed294eb0588efca936be15
Beliars <2beliars@gmail.com>
2017-06-21 11:48:17 +0300

    tslint errors fix

--- src/app/pages/results/results.ts
+++ src/app/pages/results/results.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavParams, IonicPage } from 'ionic-angular';
-import { QuestionPage } from '../question/question';
 import { RoutingService } from '../../core/services/routing.service';
 
 
@@ -11,25 +10,12 @@ import { RoutingService } from '../../core/services/routing.service';
 })
 export class ResultsPage {
   
-  resultsType: string;
-  questions;
-
   constructor(public navParams: NavParams,
               public routingService: RoutingService) {
-    
-    this.resultsType = navParams.get('resultsType');
-    
-    this.questions = [
-      { id: '111', text: 'Which dog should be the new office dog?' },
-      { id: '222', text: 'What name should be the new office dog?' },
-      { id: '333', text: 'What\'s going on?' }
-    ];
+
   }
   
   ngOnInit() {
   }
-  
-  toQuestionPage(resultsType, id, action) {
-    this.routingService.pushPage(QuestionPage, {resultsType, id, action});
-  }
+
 }
