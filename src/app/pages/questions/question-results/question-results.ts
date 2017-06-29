869b968a09d592594f23942176eaa399566a0bd4
Beliars <2beliars@gmail.com>
2017-06-29 17:38:10 +0300

    question results update with charts

--- src/app/pages/questions/question-results/question-results.ts
+++ src/app/pages/questions/question-results/question-results.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
+
+import { Chart } from 'chart.js';
 
 import * as _ from 'lodash';
 
@@ -10,8 +12,19 @@ import * as _ from 'lodash';
 })
 export class QuestionResultsPage {
   
+  @ViewChild('barCanvas') barCanvas;
+  @ViewChild('doughnutCanvas') doughnutCanvas;
   public results;
   public optimalChoice;
+  public barChart;
+  public doughnutChart;
+  public resultsLabels;
+  public resultsQuantities;
+  public chartDataset;
+  public chartOptions;
+  public colors;
+  public chartSwitched = false;
+  
   
   constructor() {
   }
@@ -40,9 +53,66 @@ export class QuestionResultsPage {
           quantity: 4
         }
       ];
-     
-    this.optimalChoice = _.maxBy(this.results, 'quantity');
     
+    this.optimalChoice = _.maxBy(this.results, 'quantity');
+    this.resultsLabels = this.results.map(result => result.text);
+    this.resultsQuantities = this.results.map(result => result.quantity);
+    this.colors = [
+      'rgba(56,142,60, 0.7)',
+      'rgba(54,162,235, 0.7)',
+      'rgba(245,124,0, 0.7)',
+      'rgba(255,206,86, 0.7)',
+      'rgba(211,47,47, 0.7)',
+      'rgba(153,102,255, 0.7)',
+      'rgba(255,159,64, 0.7)',
+      'rgba(75,192,192, 0.7)',
+      'rgba(81,45,168, 0.7)',
+      'rgba(255,99,132, 0.7)',
+      'rgba(93,64,55, 0.7)',
+      'rgba(69,90,100, 0.7)',
+      'rgba(175,180,43, 0.7)',
+    ];
+    this.chartDataset = {
+      labels: this.resultsLabels,
+      datasets: [{
+        labels: this.resultsLabels,
+        data: this.resultsQuantities,
+        backgroundColor: this.colors,
+        borderColor: this.colors,
+        borderWidth: 1
+      }]
+    };
+    
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: this.chartDataset,
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+    
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      type: 'doughnut',
+      data: this.chartDataset,
+      options: {
+        legend: {
+          onClick: false,
+          position: 'bottom'
+        }
+      }
+    });
   }
-
+  
+  onSwitchChart() {
+    this.chartSwitched = !this.chartSwitched;
+  }
 }
