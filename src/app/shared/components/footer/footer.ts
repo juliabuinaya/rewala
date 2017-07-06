999bc61f3b99d8dab64f02ceb80c8ee82fb89311
Beliars <2beliars@gmail.com>
2017-07-06 15:52:08 +0300

    tslint errors fix

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -5,7 +5,6 @@ import { RoutingService } from '../../../core/services/routing.service';
 
 import { CreateQuestionSettingsPage } from '../../../pages/create-question/create-question-settings/create-question-settings';
 import { QuestionsListPage } from '../../../pages/questions/questions-list/questions-list';
-import { QuestionResultsPage } from '../../../pages/questions/question-results/question-results';
 import { DashboardPage } from '../../../pages/dashboard/dashboard';
 import { SettingsPage } from '../../../pages/settings/settings';
 import { CreateQuestionGroupsPage } from '../../../pages/create-question/create-question-groups/create-question-groups';
