97bec154677d3d2a2075ae4d3079d4e871512bfd
Beliars <2beliars@gmail.com>
2017-07-17 13:41:52 +0300

    tslint errors fix

--- src/app/core/services/socket.service.ts
+++ src/app/core/services/socket.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, Inject } from "@angular/core";
-import { Observable } from "rxjs";
 
 import { APP_CONFIG, AppConfig } from "../../app.config";
 import * as io from "socket.io-client";
